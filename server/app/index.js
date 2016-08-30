2a2b779093ebf3b834f7eaa65b3dca2b847c32b2
jyyeh77 <jyyeh77@gmail.com>
2016-08-30 16:01:45 -0400

    halfway

--- server/app/index.js
+++ server/app/index.js
@@ -2,14 +2,30 @@
 
 var app = require('express')();
 var path = require('path');
+var session = require('express-session');
 
+app.use(session({
+	secret: 'KinisKing'
+}))
 app.use(require('./logging.middleware'));
 
 app.use(require('./request-state.middleware'));
 
 app.use(require('./statics.middleware'));
 
+app.use(function (req, res, next) {
+	console.log('session', req.session);
+	next();
+});
+
+app.use('/api', function (req, res, next) {
+	if (!req.session.counter) req.session.counter = 0;
+	console.log('counter', ++req.session.counter);
+	next();
+});
+
 app.use('/api', require('../api/api.router'));
+
 
 var validFrontendRoutes = ['/', '/stories', '/users', '/stories/:id', '/users/:id', '/signup', '/login'];
 var indexPath = path.join(__dirname, '..', '..', 'public', 'index.html');
